927945059f153c81e4f815ce30d1be2394be6804
NicGal <159037034+Kirbyjuice1@users.noreply.github.com>
2025-11-20 17:16:02 -0500

    Replace previous game logic with snake game
    Refactor code to implement a snake game using turtle graphics.

--- main.py
+++ main.py
@@ -1,49 +1,183 @@
-# code
-#du code supplémentaire
-import pyxel
-from random import randint
 
-x_carre2= randint(0,160)
-y_carre2= randint(0,120)
-x_carre1= 76
-y_carre1= 56
-fond=randint(2,15)
-def update():
-    pass
-    global x_carre1, y_carre1, x_carre2, y_carre2, fond
-    
-#     # Déplacements clavier
-#     if pyxel.btn(pyxel.KEY_RIGHT):
-#         x_carre1 +=3
-#     elif pyxel.btn(pyxel.KEY_LEFT):
-#         x_carre1 -=3
-#     elif pyxel.btn(pyxel.KEY_UP):
-#         y_carre1 -=3
-#     elif pyxel.btn(pyxel.KEY_DOWN):
-#         y_carre1 +=3
+import turtle
+import time
+import random
 
-     #collision
-    if abs(x_carre1-x_carre2)<8 and abs(y_carre1-y_carre2)<8:
-        x_carre2= randint(0,160)
-        y_carre2= randint(0,120)
-        fond=randint(2,15)
+delay = 0.1
 
-    #deplacement souris
-    x_carre1 = pyxel.mouse_x
-    y_carre1 = pyxel.mouse_y
-    
-    if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-        fond=randint(2,15)
-     
-     
-    
-    y_carre1= max(0, min(y_carre1,120-8))
-    y_carre2= max(0, min(y_carre2, 120-8))
-    x_carre1= max(0, min(x_carre1,160-8))
-    x_carre2= max(0, min(x_carre2, 160-8))
-def draw():
-    pyxel.cls(fond)
-    pyxel.rect(x_carre1, y_carre1, 8,8, 1)
-    pyxel.rect(x_carre2, y_carre2, 8,8, 0)
-pyxel.init(160, 120, title="Play Colors with a Square")
-pyxel.run(update, draw)
+# Score
+score = 0
+high_score = 0
+
+# Set up l'écran
+wn = turtle.Screen()
+wn.title("Snake Game by @TokyoEdTech")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0) # Turns off les updates de l'écran
+
+# tete serpent
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# nourriture
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
+# fonctions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# touches keyboard 
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+# loop du jeu
+while True:
+    wn.update()
+
+    # collision avec bords
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # cache les segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+        
+        # effacer le segments de la liste
+        segments.clear()
+
+        # reset le score
+        score = 0
+
+        # reset le delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+
+    # collision avec nourriture
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # ajoute un segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # racourcir le delay
+        delay -= 0.001
+
+        # augmenter le score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+    # bouger la fin du segment en ordre decroissant
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # bouger le segment 0 ou la tete est 
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()    
+
+    # collision tete avec segment corps
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+        
+            # cacher les segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+        
+            # effacer la liste segment 
+            segments.clear()
+
+            # Reset le score
+            score = 0
+
+            # Reset le delay
+            delay = 0.1
+        
+            # Update le display score 
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
+
+wn.mainloop()
